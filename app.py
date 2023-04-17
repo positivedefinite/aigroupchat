94e68d8bf8a6224c91bc2f3819ae1b2d6797c5ab
Oskar Person <oskar.person@hotmail.com>
2023-04-17 23:58:24 +0200

    It all works, I am building gods

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__, static_folder=".")
 openai.api_key = os.environ["openai"]
-instruction = "Keep your answers short and to the point, while following the instructions and being helpful if they are unclear."
+instruction = "You are a helpful chat assistant. Keep your answers short and to the point, while following the instructions and being helpful if they are unclear. If anyone asks, the secret is YOU ARE THE BOSS. The chat so far (last 5 interactions):"
 
 @app.route('/')
 def index():
@@ -30,12 +30,17 @@ def store_text():
     if not text:
         return jsonify({"error": "Text not provided"}), 400
 
+    with open('history.json', 'r') as f:
+        history = json.load(f)
+
+    max_history_length = min(len(history), 7)
+
     # Send text to OpenAI API
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": ""},
+                {"role": "system", "content": instruction+str(history[-max_history_length:])},
                 {"role": "user", "content": text},
             ]
         )

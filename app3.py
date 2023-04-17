164395c31bb21836ff94d3dda33387f1af928a70
Oskar Person <oskar.person@hotmail.com>
2023-04-17 23:11:45 +0200

    App3 now fetches a response from OpenAI

--- app3.py
+++ app3.py
@@ -1,8 +1,10 @@
 # app.py
-import json
+import json, os, openai
 from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__, static_folder=".")
+openai.api_key = os.environ["OPENAI_API_KEY"]
+instruction = "Keep your answers short and to the point, while following the instructions and being helpful if they are unclear."
 
 @app.route('/')
 def index():
@@ -14,20 +16,34 @@ def store_text():
     if not text:
         return jsonify({"error": "Text not provided"}), 400
 
-    with open('history3.json', 'a+') as f:
+    # Send text to OpenAI API
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": ""},
+                {"role": "user", "content": "Hello?"},
+            ]
+        )
+        openai_response = response.choices[0].message.content
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    # Store text and OpenAI API response in history.json
+    with open('history.json', 'a+') as f:
         f.seek(0)
         try:
             history = json.load(f)
         except json.JSONDecodeError:
             history = []
 
-        history.append(text)
+        history.append({"input": text, "response": openai_response})
 
         f.seek(0)
         f.truncate()
         json.dump(history, f)
 
-    return jsonify({"message": "Text stored successfully"}), 200
+    return jsonify({"message": f"Text stored successfully with history {history}"}), 200
 
 
 if __name__ == '__main__':
